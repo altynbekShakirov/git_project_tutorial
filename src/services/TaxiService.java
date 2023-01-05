b36cda455ead70b6589be58974c622e5442897e1
КМ <kkutman18@gmail.com>
2023-01-05 16:32:51 +0600

    d

--- src/services/TaxiService.java
+++ src/services/TaxiService.java
@@ -1,4 +1,16 @@
 package services;
 
+import classes.Taxi;
+import enums.TaxiType;
+
+import java.util.List;
+import java.util.Map;
+
 public interface TaxiService {
+    StringBuilder add(Taxi taxi);
+    StringBuilder add(List<Taxi> taxis);
+    List<Taxi> findByInitialLetter(String model);
+    Map<TaxiType, List<Taxi>> grouping();
+    List<Taxi> filterByTaxiType(String taxiType);
+    void update(Long id);
 }
